0f74a850e9c4fde3715d7dd59f99f73c5276fbe2
root <root@localhost.localdomain>
2018-11-01 11:35:18 +0530

    Flask Bcrypt committed successfully

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -3,17 +3,20 @@ import os  # new
 
 class BaseConfig:
     """Base configuration"""
+    DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
     SECRET_KEY = 'my_precious'
     DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
+    BCRYPT_LOG_ROUNDS = 13
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
-    DEBUG_TB_ENABLED = True  # new
+    DEBUG_TB_ENABLED = True
+    BCRYPT_LOG_ROUNDS = 4  # new
     # pass
 
 
@@ -21,9 +24,10 @@ class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
-
+    BCRYPT_LOG_ROUNDS = 4
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
     # pass
